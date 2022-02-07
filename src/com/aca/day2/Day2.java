c2b338eae3cd9342e87e30a19cb43e5c08a144aa
ZohrabJS <zoro.77zz777@mail.ru>
2022-02-07 16:20:05 +0400

    Day 2 Day 3

---
+++ src/com/aca/day2/Day2.java
@@ -0,0 +1,107 @@
+package com.aca.day2;
+
+import java.util.Scanner;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Task 2
+//        System.out.println("Input minutes");
+//        int minutes = scanner.nextInt();
+//        int hours = minutes / 60;
+//        System.out.println("Your minutes in hours - " + hours + " hours " + minutes % 60 + " minutes");
+        //Task3
+//        int sumOdd=1+3+5+7;
+//        int sumEven=2+4+6+8;
+//        System.out.println("the first 4 consecutive odd numbers "+sumOdd);
+//        System.out.println("the first 4 consecutive even numbers "+sumEven);
+        //Task4
+//        System.out.println("Input a");
+//        int a=scanner.nextInt();
+//        System.out.println("Input b");
+//        int b=scanner.nextInt();
+//        System.out.println("a*b="+(a*b));
+        //Task5
+//        System.out.println("Input a");
+//        int a=scanner.nextInt();
+//        System.out.println("Input b");
+//        int b=scanner.nextInt();
+//        System.out.println("Input c");
+//        int c=scanner.nextInt();
+//        System.out.println("Sum of num "+(a+b+c));
+//        System.out.println("Avg of num "+(a+b+c)/3);
+        //Task6
+//        System.out.println("Input a");
+//        int a= scanner.nextInt();
+//        System.out.println("Input b");
+//        int b=scanner.nextInt();
+//        System.out.println("right-angled triangle- "+(a*b)/2);
+//        System.out.println("Input a");
+//        a= scanner.nextInt();
+//        System.out.println("square- "+a*a);
+//        System.out.println("Input a");
+//        a= scanner.nextInt();
+//        System.out.println("Input b");
+//        b=scanner.nextInt();
+//        System.out.println("rectangle- "+a*b);
+        //Task7
+//        byte b=10;
+//        short c=15;
+//        int a=11;
+//        System.out.println("average : "+(a+b+c)/3);
+//        System.out.println("byte variable-"+b);
+//        System.out.println("short variable-"+c);
+//        System.out.println("int variable-"+a);
+        //Task8
+//        System.out.println("Sn  = 3n*n - 12n");
+//        System.out.println("S10="+((3*10*10)-12*10));
+//        System.out.println("S15="+((3*15*15)-12*15));
+        //Task9
+//        System.out.println("Input num");
+//        int num=scanner.nextInt();
+//        System.out.println("last digit of num: "+num%10);
+        //Task11
+//        System.out.println("Input a");
+//        int a= scanner.nextInt();
+//        System.out.println("Input b");
+//        int b=scanner.nextInt();
+//        System.out.println("Remainder from  a to b- "+a/b+" , "+a%b);
+        //Task12
+//        Random random=new Random();
+//        System.out.println(random.nextInt(100));
+        //Task13
+//        Random random=new Random();
+//        System.out.println(random.nextInt(16));
+        //Task14
+//        Random random=new Random();
+//        System.out.println(random.nextInt(15,30));
+        //Task15
+//        System.out.println("Input number");
+//        int num=scanner.nextInt();
+//        boolean isOdd=(num%2==1);
+//        System.out.println(isOdd? "odd":"Not odd");
+        //Task16
+//        System.out.println("Input 3-digit number");
+//        int n= scanner.nextInt();
+//        System.out.println("second digit is "+ (n%100)/10);
+        //Task17
+//        System.out.println("Input 3-digit number");
+//        int n= scanner.nextInt();
+//        System.out.println("first digit is "+ n/100);
+        //Task18
+//        System.out.println("Input 3-digit number");
+//        int n= scanner.nextInt();
+//        System.out.println("sum of all 3 digits "+ ((n%100)/10+n%10+n/100));
+        //Task19
+//        System.out.println("Input a");
+//        int a= scanner.nextInt();
+//        System.out.println("Input b");
+//        int b= scanner.nextInt();
+//        int c=a;
+//        a=b;
+//        b=c;
+//        System.out.println("a="+a);
+//        System.out.println("b="+b);
+    }
+}
---
+++ src/com/aca/day3/Day3.java
@@ -0,0 +1,119 @@
+package com.aca.day3;
+
+import java.util.Scanner;
+
+public class Day3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Task 1
+        /*
+        System.out.println("Pleas Input variable");
+        byte a = scanner.nextByte();
+        short b=a;
+        System.out.println("Byte variable = "+a +" Short variable = "+b);
+        int c=b;
+        System.out.println("Short variable = "+b +" Int variable = "+c);
+        long d=c;
+        System.out.println("Int variable = "+c +" Long variable = "+d);
+        float e=d;
+        System.out.println("Long variable = "+d +" Float variable = "+e);
+        double f=e;
+        System.out.println("Float variable = "+e +" Double variable = "+f);
+        */
+        //Task 2
+        /*
+        System.out.println("Pleas Input variable");
+        double a = scanner.nextDouble();
+        float b= (float) a;
+        System.out.println("Double variable = "+a +" Float variable = "+b);
+        long c= (long) b;
+        System.out.println("Float variable = "+b +" Long variable = "+c);
+        int d= (int) c;
+        System.out.println("Long variable = "+c +" Int variable = "+d);
+        short e= (short) d;
+        System.out.println("Int variable = "+d +" Short variable = "+e);
+        byte f= (byte) e;
+        System.out.println("Short variable = "+e +" Byte variable = "+f);
+        */
+        //Task 3
+        /*
+        System.out.println("Pleas input temperature in Fahrenheit");
+        double f= scanner.nextDouble();
+        double t = ((f-32)*5/9);
+        float t1=(float) t;
+        int t2=(int) t;
+        System.out.println("Result in float = "+t1+"C");
+        System.out.println("Result in int = "+t2+"C");
+        */
+        //Task 4
+        /*
+        System.out.printf("Input radius ");
+        double r= scanner.nextDouble();
+        double area= PI *r*r;
+        double circle=2*PI*r;
+        System.out.println("Area = "+area);
+        System.out.println("Circle = "+circle);
+         */
+        //Task 5
+        /*
+        System.out.println("1/3 in double = "+(double)1/3);
+        System.out.println("1/3 in float = "+(float)1/3);
+        */
+        //Task 6
+        /*
+        System.out.println("Your name");
+        char a='A';
+        char r='r';
+        char b='a';
+        int age=1996;
+        System.out.println(a +""+ r+""+b+" "+age);
+         */
+        //Task 7
+        /*
+        System.out.println("Input char variable");
+        char a=scanner.next().charAt(0);
+        int b=(int)a;
+        System.out.println("Cast char to int = "+b);
+        */
+        //Task 8
+        /*
+        char a='A';
+        char r='r';
+        char b='a';
+        double averaje=(a+r+b)/3;
+        System.out.println("Ara -- "+averaje);
+         */
+        //Task 9
+        /*
+        System.out.println("Input value in range 32 to 127");
+        int a=scanner.nextInt();
+        char b=(char)a;
+        System.out.println(a+"-"+b);
+         */
+        //Task 10
+        /*
+        String a="Str";
+        String b="ing";
+        System.out.println(a+b);
+        */
+        //Task 11
+        /*
+        String b="5";
+        int a=10;
+        int c=5;
+        System.out.println("Int(5)+Int(10) = "+ (c+a));
+        System.out.println("String(5)+Int(10) = "+(b+a));
+        */
+        //Task 12
+        /*
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        System.out.println(txt);
+        System.out.println( "\nNew line");
+        System.out.println("\tTab");
+        System.out.println("\r Carriage return");
+        System.out.println("\b backslash");
+
+         */
+
+    }
+}
