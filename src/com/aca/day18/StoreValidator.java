a044123fd194e68e6c2b9c031ebba1ceb460cc12
ZohrabJS <zoro.77zz777@mail.ru>
2022-03-02 13:29:54 +0400

    Day18 add validator

--- src/com/aca/day18/StoreValidator.java
+++ src/com/aca/day18/StoreValidator.java
@@ -5,7 +5,7 @@ public class StoreValidator {
         return worker >= 2 && worker <= 50;
     }
     public static boolean nameValidator(String name){
-        return name.length() >= 3;
+        return name!=null && name.length() >= 3;
     }
     public static boolean phoneValidator(int phoneNumber ){
         return phoneNumber >= 10000000 && phoneNumber <= 99999999;
